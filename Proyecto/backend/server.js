0fed01a7f43fc5fcd7ae86fa2e68de22829547f1
BrayanForeroVega <brayan.forero-v@uniminuto.edu.co>
2025-09-20 20:30:53 -0500

    cambios en server

--- Proyecto/backend/server.js
+++ Proyecto/backend/server.js
@@ -1,24 +1,15 @@
 const express = require('express');
-   const passport = require('passport');
-   const app = express();
-   const http = require('http');
-   const server = http.createServer(app);
-   const logger = require('morgan');
-   const cors = require('cors');
-   const usersRoutes = require('./routes/userRoutes');
-   const port = process.env.PORT || 3000;
-   app.use(logger('dev'));
-   app.use(express.json());
-   app.use(express.urlencoded({ extended: true }));
-   app.use(cors());
-   app.use(passport.initialize());
-   app.use(passport.session());
-   require('./config/passport')(passport);
-   app.disable('x-powered-by');
-   app.set('port', port);
-   usersRoutes(app);
+const http = require('http');
+const logger = require('morgan');
+const cors = require('cors');
+const usersRoutes = require('./routes/userRoutes');
 
-   // Middlewares globales
+const app = express();
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3000;
+
+// Middlewares globales
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -26,6 +17,22 @@ app.use(cors());
 
 // Rutas
 app.use('/api/users', usersRoutes);
+
+// Configuración extra
+app.disable('x-powered-by');
+app.set('port', port);
+
+// Servidor
+server.listen(port, '172.20.240.1' || 'localhost', function () {
+  console.log(
+    'App node.js ' +
+      process.pid +
+      ' ejecutando en ' +
+      server.address().address +
+      ':' +
+      server.address().port
+  );
+});
 
 // Endpoints de prueba
 app.get('/', (req, res) => {
@@ -41,20 +48,3 @@ app.use((err, req, res, next) => {
   console.log(err);
   res.status(err.status || 500).send(err.stack);
 });
-
-   server.listen(3000, '172.20.240.1' || 'localhost', function(){
-     console.log('App node.js ' + process.pid + ' ejecutando en ' + server.address().address + ':' + server.address().port);
-   });
-   app.get('/', (req, res) => {
-     res.send('Ruta raíz del Backend');
-   });
-   app.get('/test', (req, res) => {
-     res.send('Ruta TEST');
-   });
-   app.use((err, req, res, next) => {
-     console.log(err);
-     res.status(err.status || 500).send(err.stack);
-   });
-
-   // Exportamos la app para que la use index.js
-module.exports = app;
